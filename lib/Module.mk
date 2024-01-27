hramrach/lm-sensors

commit c829b4c7fb8ccec9480d670ea9d2f5a04b035b45

Add LDFLAGS handling similar to CFLAGS

lib/Module.mk
@@ -69,7 +69,7 @@ LIBHEADERFILES := $(MODULE_DIR)/error.h $(MODULE_DIR)/sensors.h
 
 # How to create the shared library
 $(MODULE_DIR)/$(LIBSHLIBNAME): $(LIBSHOBJECTS) $(LIB_DIR)/libsensors.map
-	$(CC) -shared $(LDFLAGS) -Wl,--version-script=$(LIB_DIR)/libsensors.map -Wl,-soname,$(LIBSHSONAME) -o $@ $(LIBSHOBJECTS) -lc -lm
+	$(CC) -shared $(ALL_LDFLAGS) -Wl,--version-script=$(LIB_DIR)/libsensors.map -Wl,-soname,$(LIBSHSONAME) -o $@ $(LIBSHOBJECTS) -lc -lm
 
 $(MODULE_DIR)/$(LIBSHSONAME): $(MODULE_DIR)/$(LIBSHLIBNAME)
 	$(RM) $@

Makefile
@@ -63,8 +63,6 @@ DATADIR := $(PREFIX)/share
 # ZSH completion files will be installed.
 ZSHCOMPDIR := $(DATADIR)/zsh/site-functions
 
-EXLDFLAGS := -Wl,-rpath,$(LIBDIR)
-
 # You should not need to change this. It is the directory into which the
 # executable program files will be installed. BINDIR for programs that are
 # also useful for normal users, SBINDIR for programs that can only be run
@@ -170,6 +168,10 @@ ARCPPFLAGS := -DETCDIR="\"$(ETCDIR)\"" $(ALL_CPPFLAGS)
 ARCFLAGS := $(ALL_CFLAGS)
 LIBCPPFLAGS := -DETCDIR="\"$(ETCDIR)\"" $(ALL_CPPFLAGS)
 LIBCFLAGS := -fpic -D_REENTRANT $(ALL_CFLAGS)
+
+ALL_LDFLAGS := $(LDFLAGS)
+
+EXLDFLAGS := -Wl,-rpath,$(LIBDIR) $(ALL_LDFLAGS)
 
 .PHONY: all user clean install user_install uninstall user_uninstall
 

commit d8b1c8f5a2b7803c39aeb912769fc6bf1049bba4

Fix build with extra libraries
Fixes: c829b4c7 ("Add LDFLAGS handling similar to CFLAGS")

lib/Module.mk
@@ -69,7 +69,7 @@ LIBHEADERFILES := $(MODULE_DIR)/error.h $(MODULE_DIR)/sensors.h
 
 # How to create the shared library
 $(MODULE_DIR)/$(LIBSHLIBNAME): $(LIBSHOBJECTS) $(LIB_DIR)/libsensors.map
-	$(CC) -shared $(ALL_LDFLAGS) -Wl,--version-script=$(LIB_DIR)/libsensors.map -Wl,-soname,$(LIBSHSONAME) -o $@ $(LIBSHOBJECTS) -lc -lm
+	$(CC) -shared -Wl,--version-script=$(LIB_DIR)/libsensors.map -Wl,-soname,$(LIBSHSONAME) -o $@ $(LIBSHOBJECTS) $(ALL_LDFLAGS) -lc -lm
 
 $(MODULE_DIR)/$(LIBSHSONAME): $(MODULE_DIR)/$(LIBSHLIBNAME)
 	$(RM) $@

prog/dump/Module.mk
@@ -43,10 +43,10 @@ all-prog-dump: $(PROGDUMPTARGETS)
 user :: all-prog-dump
 
 $(MODULE_DIR)/isadump: $(MODULE_DIR)/isadump.ro $(MODULE_DIR)/superio.ro $(MODULE_DIR)/util.ro
-	$(CC) $(EXLDFLAGS) -o $@ $^
+	$(CC) -o $@ $^ $(EXLDFLAGS)
 
 $(MODULE_DIR)/isaset: $(MODULE_DIR)/isaset.ro $(MODULE_DIR)/util.ro
-	$(CC) $(EXLDFLAGS) -o $@ $^
+	$(CC) -o $@ $^ $(EXLDFLAGS)
 
 install-prog-dump: all-prog-dump
 	$(MKDIR) $(DESTDIR)$(SBINDIR) $(DESTDIR)$(PROGDUMPMAN8DIR)

prog/sensord/Module.mk
@@ -42,7 +42,7 @@ REMOVESENSORDBIN := $(patsubst $(MODULE_DIR)/%,$(DESTDIR)$(SBINDIR)/%,$(PROGSENS
 REMOVESENSORDMAN := $(patsubst $(MODULE_DIR)/%,$(DESTDIR)$(PROGSENSORDMAN8DIR)/%,$(PROGSENSORDMAN8FILES))
 
 $(PROGSENSORDTARGETS): $(PROGSENSORDSOURCES:.c=.ro) lib/$(LIBDEP_FOR_PROGS)
-	$(CC) $(EXLDFLAGS) -o $@ $(PROGSENSORDSOURCES:.c=.ro) -Llib -lsensors -lrrd
+	$(CC) -o $@ $(PROGSENSORDSOURCES:.c=.ro) $(EXLDFLAGS) -Llib -lsensors -lrrd
 
 all-prog-sensord: $(PROGSENSORDTARGETS)
 user :: all-prog-sensord

prog/sensors/Module.mk
@@ -42,7 +42,7 @@ REMOVESENSORSZSH := $(patsubst $(MODULE_DIR)/%,$(DESTDIR)$(ZSHCOMPDIR)/%,$(PROGS
 LIBICONV := $(shell if /sbin/ldconfig -p | grep -q '/libiconv\.so$$' ; then echo \-liconv; else echo; fi)
 
 $(PROGSENSORSTARGETS): $(PROGSENSORSSOURCES:.c=.ro) lib/$(LIBDEP_FOR_PROGS)
-	$(CC) $(EXLDFLAGS) -o $@ $(PROGSENSORSSOURCES:.c=.ro) $(LIBICONV) -Llib -lsensors -lm
+	$(CC) -o $@ $(PROGSENSORSSOURCES:.c=.ro) $(LIBICONV) $(EXLDFLAGS) -Llib -lsensors -lm
 
 all-prog-sensors: $(PROGSENSORSTARGETS)
 user :: all-prog-sensors

lib/test/Module.mk
@@ -15,7 +15,7 @@ LIB_TEST_SCANNER_OBJS := \
 INCLUDEFILES += $(LIB_TEST_SOURCES:.c=.rd)
 
 $(LIB_TEST_DIR)/test-scanner: $(LIB_TEST_SCANNER_OBJS)
-	$(CC) $(EXLDFLAGS) -o $@ $(LIB_TEST_SCANNER_OBJS) -Llib
+	$(CC) -o $@ $(LIB_TEST_SCANNER_OBJS) -Llib $(EXLDFLAGS)
 
 all-lib-test: $(LIB_TEST_TARGETS)
 user :: all-lib-test